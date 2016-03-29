69e0d889c47409985c8c5e2fa22c3fb05f01eb1b
felipevicens <felipe.vicens@atos.net>
2016-03-29 10:09:35 +0200

    Added API routes of /

---
+++ helpers/sonata.rb
@@ -0,0 +1,31 @@
+class Sonata < Sinatra::Application
+
+  require 'json'
+  require 'yaml'
+
+	def api_routes
+		[
+				{
+						'uri' => '/',
+						'method' => 'GET',
+						'purpose' => 'REST API Structure and Capability Discovery'
+				},
+				{
+						'uri' => '/records/nsr/',
+						'method' => 'GET',
+						'purpose' => 'REST API Structure and Capability Discovery nsr'
+				},
+				{
+						'uri' => '/records/vnfr/',
+						'method' => 'GET',
+						'purpose' => 'REST API Structure and Capability Discovery vnfr'
+				},
+				{
+						'uri' => '/catalogues/',
+						'method' => 'GET',
+						'purpose' => 'REST API Structure and Capability Discovery catalogues'
+				}
+		]
+	end
+
+end
