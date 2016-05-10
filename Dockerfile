cc087d100f89f4a5a732724ed9d16eb50649a8cd
felipevicens <felipe.vicens@atos.net>
2016-05-10 11:04:22 +0200

    Adding Unit Test NS Repo

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,10 @@ FROM ruby:2.2.3
 RUN mkdir -p /app
 COPY Gemfile /app/
 WORKDIR /app
+RUN bundle config --global jobs 7
 RUN bundle install
 COPY . /app
-RUN rake yard
+#RUN rake yard
 ENV PORT 4567
 EXPOSE 8808
 EXPOSE 4567
---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+# spec/spec_helper.rb
+require 'rack/test'
+require 'rspec'
+require 'webmock/rspec'
+
+ENV['RACK_ENV'] ||= 'test'
+
+WebMock.allow_net_connect!
+
+$: << File.expand_path('../..', __FILE__)
+require './main'
+
+def app
+  SonataNsRepository
+end
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+  config.mock_with :rspec
+  config.include WebMock::API
+end
