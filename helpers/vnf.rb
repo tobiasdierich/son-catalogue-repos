242b4ba74af1f67b70161a1b742b74cc76e2e9cd
Daniel Guija <dani.guija@i2cat.net>
2016-07-07 12:53:16 +0100

    Added license headers, updated README and cleaned code

--- helpers/vnf.rb
+++ helpers/vnf.rb
@@ -1,3 +1,29 @@
+##
+## Copyright (c) 2015 SONATA-NFV
+## ALL RIGHTS RESERVED.
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
+## Neither the name of the SONATA-NFV [, ANY ADDITIONAL AFFILIATION]
+## nor the names of its contributors may be used to endorse or promote
+## products derived from this software without specific prior written
+## permission.
+##
+## This work has been performed in the framework of the SONATA project,
+## funded by the European Commission under Grant number 671517 through
+## the Horizon 2020 and 5G-PPP programmes. The authors would like to
+## acknowledge the contributions of their colleagues of the SONATA
+## partner consortium (www.sonata-nfv.eu).
 
 # @see VNFRepository
 class SonataVnfRepository < Sinatra::Application
@@ -37,7 +63,7 @@ class SonataVnfRepository < Sinatra::Application
       logger.error "JSON validating: #{e.to_s}"
       return e.to_s + "\n"
     end
-    return nil
+    nil
   end
   # Checks if a YAML message is valid
   #
@@ -75,11 +101,11 @@ class SonataVnfRepository < Sinatra::Application
       output_json = JSON.dump(input_yml)
       #output_json = JSON.dump(input_yml.to_json)
     rescue
-      logger.error "Error parsing from YAML to JSON"
+      logger.error 'Error parsing from YAML to JSON'
       end
 
     puts 'Parsing DONE', output_json
-  return output_json
+  output_json
   end
 
   # Translates a message from JSON to YAML
@@ -94,10 +120,10 @@ class SonataVnfRepository < Sinatra::Application
     begin
       output_yml = YAML.dump(JSON.parse(input_json))
     rescue
-      logger.error "Error parsing from JSON to YAML"
+      logger.error 'Error parsing from JSON to YAML'
       end
 
-    return output_yml
+    output_yml
   end
 
   # Builds an HTTP link for pagination
@@ -108,17 +134,17 @@ class SonataVnfRepository < Sinatra::Application
     link = ''
     # Next link
     next_offset = offset + 1
-    next_vnfs = Vnf.paginate(:page => next_offset, :limit => limit)
+    next_vnfs = Vnf.paginate(page: next_offset, limit: limit)
     begin
       link << '<localhost:4011/virtual-network-functions?offset=' + next_offset.to_s + '&limit=' + limit.to_s + '>; rel="next"' unless next_vnfs.empty?
     rescue
-      logger.error "Error Establishing a Database Connection"
+      logger.error 'Error Establishing a Database Connection'
     end
 
     unless offset == 1
       # Previous link
       previous_offset = offset - 1
-      previous_vnfs = Vnf.paginate(:page => previous_offset, :limit => limit)
+      previous_vnfs = Vnf.paginate(page: previous_offset, limit: limit)
       unless previous_vnfs.empty?
         link << ', ' unless next_vnfs.empty?
         link << '<localhost:4011/virtual-network-functions?offset=' + previous_offset.to_s + '&limit=' + limit.to_s + '>; rel="last"'
@@ -132,17 +158,17 @@ class SonataVnfRepository < Sinatra::Application
     link = ''
     # Next link
     next_offset = offset + 1
-    next_vnfs = Vnf.paginate(:page => next_offset, :limit => limit)
+    next_vnfs = Vnf.paginate(page: next_offset, limit: limit)
     begin
       link << '<localhost:4011/virtual-network-functions/name/' + name.to_s + '?offset=' + next_offset.to_s + '&limit=' + limit.to_s + '>; rel="next"' unless next_vnfs.empty?
     rescue
-      logger.error "Error Establishing a Database Connection"
+      logger.error 'Error Establishing a Database Connection'
     end
 
     unless offset == 1
       # Previous link
       previous_offset = offset - 1
-      previous_vnfs = Vnf.paginate(:page => previous_offset, :limit => limit)
+      previous_vnfs = Vnf.paginate(page: previous_offset, limit: limit)
       unless previous_vnfs.empty?
         link << ', ' unless next_vnfs.empty?
         link << '<localhost:4011/virtual-network-functions/name/' + name.to_s + '?offset=' + previous_offset.to_s + '&limit=' + limit.to_s + '>; rel="last"'
